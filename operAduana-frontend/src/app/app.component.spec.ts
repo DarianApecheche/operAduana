83939dfae8e62b3ce6f5cb87085483ddb086fe0b
DarianApecheche <kirinogenshin@gmail.com>
2025-04-28 07:41:33 +0200

    feat(frontend actualizado)

--- operAduana-frontend/src/app/app.component.spec.ts
+++ operAduana-frontend/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'operAduana' title`, () => {
+  it(`should have the 'operAduana-frontend' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('operAduana');
+    expect(app.title).toEqual('operAduana-frontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, operAduana');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, operAduana-frontend');
   });
 });
